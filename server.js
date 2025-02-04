cc559dbd930c80bb8f75dea30cd1632587b8d285
Umar Tahir <umarstock2@gmail.com>
2025-02-05 03:02:45 +0500

    modify server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 // Initialize Express App
 const app = express();
@@ -12,13 +13,10 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect(
-    "mongodb+srv://umarstock2:mcclane33@cluster0.h1wuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Database connection error:", err));
 
@@ -64,7 +62,7 @@ app.post("/submit-form", async (req, res) => {
 });
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
